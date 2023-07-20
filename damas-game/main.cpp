fcfdbe667a1cdc6cc314e9c5cbf2e4cae91cc6f7
Safira <safiraaabarbosaaa@gmail.com>
2023-07-19 23:34:17 -0300

    add: preenchendo tabuleiro

--- damas-game/main.cpp
+++ damas-game/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#define VAZIO = 0
+#define BRANCA = 1
+#define PRETA = 2
 
 using namespace std;
 
 void escreverArquivo(string diretorio, string conteudo);
 string lerArquivo(string diretorio);
+void inicializarTabuleiro();
 
 const string diretorio = "save/dados.txt"; //a cada pasta coloca-se uma nova barra e no final deve ser o nome do arquivo e a extensão.
+const int TAM_TABULEIRO = 8;
+
+int tabuleiro [TAM_TABULEIRO][TAM_TABULEIRO] = {VAZIO};
 
 int main()
 {
+    inicializarTabuleiro();
     return 0;
 
 }
 
+void inicializarTabuleiro()
+{
+    //lado das peças brancas
+    for(int linha=0; linha<3; linha++)
+        for(int coluna=0; coluna<TAMTABULEIRO; coluna++)
+            if((linha + coluna)% 2 == 0)
+                tabuleiro[linha][coluna] = BRANCA;
+
+    //lado das peças pretas
+    for(int linha=5; linha<8; linha++)
+        for(int coluna=0; coluna<TAMTABULEIRO; coluna++)
+            if((linha + coluna)% 2 == 1)
+                tabuleiro[linha][coluna] = PRETA;
+
+}
 void escreverArquivo(string diretorio, string conteudo)
 {
     fstream arquivo(diretorio, ios::out); //fstream funciona como tipo de arquivo
