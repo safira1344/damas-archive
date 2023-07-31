8e2b477633dd849d56b6df6ed54b468093f3ee7b
Safira <safiraaabarbosaaa@gmail.com>
2023-07-30 23:17:14 -0300

    release: v.beta de jogo de damas

--- damas-game/main.cpp
+++ damas-game/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <iomanip>
+#include "gconio.h"
 
 #define VAZIO 0
 #define BRANCA 1
@@ -10,88 +11,202 @@
 
 using namespace std;
 
-struct Jogador
+typedef struct jogador
 {
     string nome;
     int vitorias;
     int quantidadePartidas;
-};
+} Jogador;
 
+// Funções de arquivo
 void escreverArquivo(string diretorio, string conteudo);
 string lerArquivo(string diretorio);
-void escreverArquivoBinario(string diretorio, string conteudo);
-string lerArquivoBinario(string diretorio);
+void salvarTabuleiro();
+void carregarTabuleiro();
+void salvarPlacar(Jogador *Jogadores);
 
-void inicializarTabuleiro();
+// Funções de interface
+void printarPlacar();
 void printaTabuleiro();
+void mostrarGanhador();
+void tutorial();
+
+// Funções de jogo
+void menu();
 bool movimentoValido(int linhaOrigem, int linhaChegada, int colunaOrigem, int colunaChegada, bool isMovimentando);
 bool movimentoValido(int linhaOrigem, int linhaChegada, int colunaOrigem, int colunaChegada);
 void movimentar();
 bool verificaVitoria();
-void iniciarJogo();
 void jogando();
-void tutorial();
-void salvarPlacar();
-void printarPlacar();
-void menu();
-void mostrarGanhador();
-bool comerPeca(int linhaCaptura, int colunaCaptura);
 
-const string diretorio = "save/dados.txt"; // a cada pasta coloca-se uma nova barra e no final deve ser o nome do arquivo e a extensão.
+// Funções de inicialização
+void inicializarTabuleiro();
+void iniciarJogo();
+
 const int TAM_TABULEIRO = 8;
 
 int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO] = {VAZIO};
 int jogadorAtual;
 
+void salvarTabuleiro()
+{
+    // Percorrer o tabuleiro e salvar na variavel conteudo
+    string conteudo = "";
+    for (int linha = 0; linha < TAM_TABULEIRO; linha++)
+    {
+        for (int coluna = 0; coluna < TAM_TABULEIRO; coluna++)
+        {
+            conteudo += to_string(tabuleiro[linha][coluna]); 
+        }
+    }
+    escreverArquivo("save/tabuleiro.txt", conteudo);
+}
+
+void carregarTabuleiro()
+{
+    // Ler o arquivo e salvar na variavel conteudo
+    string conteudo = lerArquivo("save/tabuleiro.txt");
+    int contador = 0;
+
+    // Percorrer a string conteudo e salvar no tabuleiro
+    for (int linha = 0; linha < TAM_TABULEIRO; linha++)
+    {
+        for (int coluna = 0; coluna < TAM_TABULEIRO; coluna++)
+        {
+            tabuleiro[linha][coluna] = (int)conteudo[contador];
+            contador++;
+        }
+    }
+}
+
 int main()
 {
-    // Ler o nome dos jogadores
-    // Buscar no arquivo de placar se já existe um jogador com o mesmo nome
-    // Se existir, carregar os dados dele
-    // Se não existir, criar um novo jogador
-
     menu();
+    inicializarTabuleiro();
 
-    int travaFechamentoDaJanelaDoTerminal; // Variável para impedir que o programa feche após a execução
-    cin >> travaFechamentoDaJanelaDoTerminal;
+    getch();
     return 0;
 }
 
 void menu()
 {
-    cout << "Bem vindo ao jogo de Damas!" << endl;
-    cout << "0 - Tutorial" << endl;
-    cout << "1 - Jogar" << endl;
-    cout << "2 - Placar" << endl;
-    cout << "3 - Sair" << endl;
-    cout << "Digite a opção desejada: ";
-    int opcao;
-    cin >> opcao;
-    switch (opcao)
+    Jogador *Jogadores = new Jogador[2];
+    textcolor(GREEN);
+
+    cout << "Digite o nome do jogador 1: ";
+    cin >> Jogadores[0].nome;
+    Jogadores[0].vitorias = 0;
+    Jogadores[0].quantidadePartidas = 0;
+
+    cout << "Digite o nome do jogador 2: ";
+    cin >> Jogadores[1].nome;
+    Jogadores[1].vitorias = 0;
+    Jogadores[1].quantidadePartidas = 0;
+
+    salvarPlacar(Jogadores);
+
+    while (true)
     {
-    case 0:
-        tutorial();
-        break;
-    case 1:
-        iniciarJogo();
-        break;
-    case 2:
-        printarPlacar();
-        break;
-    case 3:
-        exit(0);
-        break;
+
+        textbackground(BLACK);
+        clrscr();
+        // textbackground(RED);
+        // textcolor(LIGHTGRAY);
+        gotoxy(34, 7);
+        cout << (char)(201);
+        for (int i = 0; i < 43; i++)
+        {
+            cout << (char)(205);
+        }
+        cout << (char)(187);
+
+        for (int i = 8; i <= 18; i++)
+        {
+            gotoxy(34, i);
+            cout << (char)(186);
+            for (int j = 0; j < 44; j++)
+            {
+                if (j == 43)
+                {
+                    cout << (char)(186);
+                    break;
+                }
+                cout << " ";
+            }
+        }
+        gotoxy(34, 19);
+        cout << (char)(200);
+        for (int i = 0; i < 43; i++)
+        {
+            cout << (char)(205);
+        }
+        cout << (char)(188);
+
+        gotoxy(41, 9);
+        cout << "Bem vindo ao jogo de Damas! ";
+        gotoxy(38, 11);
+        cout << "0 - Tutorial";
+        gotoxy(38, 12);
+        cout << "1 - Jogar";
+        gotoxy(38, 13);
+        cout << "2 - Placar";
+        gotoxy(38, 14);
+        cout << "3 - Sair";
+        gotoxy(38, 16);
+        cout << "Digite a opcao desejada: ";
+
+        int opcao;
+        cin >> opcao;
+        textbackground(BLACK);
+        // textcolor(WHITE);
+
+        switch (opcao)
+        {
+        case 0:
+            tutorial();
+            break;
+        case 1:
+            iniciarJogo();
+            break;
+        case 2:
+            printarPlacar();
+            getch();
+            break;
+        case 3:
+            clrscr();
+            gotoxy(30, 3);
+            printf("--APERTE QUALQUER TECLA PARA FECHAR O PROGRAMA--");
+            gotoxy(0, 5);
+            printf("  ___  ___ ___   ___ ___  ___     _ ___ ___ _____ \n");
+            printf(" |   \\/ __|_ _| | _ \\ _ \\/ _ \\ _ | | __/ __|_   _|\n");
+            printf(" | |) \\__ \\| |  |  _/   / (_) | || | _| (__  | |  \n");
+            printf(" |___/|___/___| |_| |_|_\\\\____/ \\__/|___\\__ | |_|  \n");
+            printf("                                                  \n");
+            gotoxy(32, 14);
+            getch();
+
+            exit(0);
+
+            break;
+        }
     }
 }
 
-void salvarPlacar()
+void salvarPlacar(Jogador *Jogadores)
 {
-    // Verificar se ja existe um jogador com o mesmo nome no arquivo de placar, se existir atualizar a pontuação dele, se não, criar ele
+    // Percorrer o vetor de jogadores e salvar no arquivo
+    string conteudo;
+    string diretorio = "save/placar.txt";
+    conteudo = Jogadores[0].nome + " " + to_string(Jogadores[0].vitorias) + "\n" + Jogadores[1].nome + " " + to_string(Jogadores[1].vitorias) + "\n";
+    escreverArquivo(diretorio, conteudo);
 }
 
 void printarPlacar()
 {
-    // Ler todas as structs do arquivo e printar ordenadamente por quantidade de vitórias
+    // Ler o arquivo e salvar na variavel conteudo
+    string diretorio = "save/placar.txt";
+    string conteudo = lerArquivo(diretorio);
+    cout << conteudo;
 }
 
 bool verificaVitoria()
@@ -125,22 +240,39 @@ bool verificaVitoria()
 
 void tutorial()
 {
-    cout << setw(50);
-    cout << "Regras" << endl;
-    cout << endl;
-    cout << setw(55);
-    cout << "Tabuleiro e Peças" << endl;
-    cout << endl;
-    cout << setw(60);
-    cout << "1- O jogo é jogado em um tabuleiro 8x8, com casas alternando entre pretas e brancas. " << endl;
-    cout << setw(60);
-    cout << "2- Cada jogador tem 12 peças, representadas por peças brancas e peças pretas." << endl;
-    cout << setw(60);
-    cout << "3- Movem-se diagonalmente para frente, promovem-se a dama na última linha. Capturas obrigatórias." << endl;
-    cout << setw(60);
-    cout << "4- Objetivo: capturar todas as peças do adversário ou bloqueá-las." << endl;
-    cout << setw(60);
-    cout << "5- Notação: letras A-H para linhas, números 0-7 para colunas. Divirta-se!" << endl;
+    clrscr();
+    // textbackground(RED);
+    // textcolor(LIGHTGRAY);
+
+    for (int i = 1; i < 20; i++)
+    {
+        gotoxy(10, i);
+        for (int j = 0; j < 100; j++)
+        {
+            cout << " ";
+        }
+    }
+
+    gotoxy(50, 2);
+    cout << "=====Regras=====";
+
+    gotoxy(20, 4);
+    cout << "Tabuleiro e Pecas";
+
+    gotoxy(12, 6);
+    cout << "1- O jogo pode ser jogado em um tabuleiro 8x8, com casas alternando entre pretas e brancas. ";
+    gotoxy(12, 8);
+    cout << "2- Cada jogador tem 12 pecas, representadas por pecas brancas e pecas pretas.";
+    gotoxy(12, 10);
+    cout << "3- Movem-se diagonalmente para frente, promovem-se a dama na ultima linha. Capturas obrigatorias.";
+    gotoxy(12, 12);
+    cout << "4- Objetivo: capturar todas as pecas do adversario ou bloquea-las.";
+    gotoxy(12, 14);
+    cout << "5- Maneira de Jogar: letras A-H para linhas, numeros 0-7 para colunas. Divirta-se!";
+    gotoxy(12, 18);
+    cout << "Pressione qualquer tecla para voltar ao menu principal... ";
+    cin.ignore();
+    getch();
 }
 
 void jogando()
@@ -151,9 +283,12 @@ void jogando()
         printaTabuleiro();
         movimentar();
         temVencedor = verificaVitoria();
-    } while (temVencedor == false);
 
+    } while (temVencedor == false);
     mostrarGanhador();
+    cout << "Pressione qualquer tecla para sair";
+    getch();
+    clrscr();
     menu();
 }
 
@@ -165,6 +300,7 @@ void mostrarGanhador()
 
 void iniciarJogo()
 {
+
     jogadorAtual = BRANCA; // Primeiro jogador sempre é o branco
     inicializarTabuleiro();
     jogando();
@@ -174,14 +310,30 @@ void movimentar()
 {
     int linhaOrigem, colunaOrigem, linhaDestino, colunaDestino;
     char letraOrigem, letraDestino;
-    cout << "Jogador " << ((jogadorAtual == BRANCA) ? "Branco" : "Preto") << ", digite a letra e numero da peça que deseja mover: ";
+
+    gotoxy(5, 27);
+    cout << "(0)(0) para Sair ";
+    gotoxy(0, 20);
+
+    // Pede a posição da peça que o jogador quer mover
+    cout << "Jogador " << ((jogadorAtual == BRANCA) ? "Branco" : "Preto") << ", digite a letra e numero da peca que deseja mover: ";
     cin >> letraOrigem >> colunaOrigem;
+
+    if (letraOrigem == '0' && colunaOrigem == 0)
+    {
+        salvarTabuleiro();
+        clrscr();
+        menu();
+    }
+    // Pede a posição da peça que o jogador quer mover
     cout << "Jogador " << ((jogadorAtual == BRANCA) ? "Branco" : "Preto") << ", digite a letra e numero do destino: ";
     cin >> letraDestino >> colunaDestino;
 
+    // Converte a letra para um número de 0 a 7 (A = 0, B = 1, etc)
     linhaOrigem = ((int)toupper(letraOrigem)) - 65;
     linhaDestino = ((int)toupper(letraDestino)) - 65;
 
+    // Verifica se o movimento é válido
     if (movimentoValido(linhaOrigem, colunaOrigem, linhaDestino, colunaDestino, true))
     {
         tabuleiro[linhaDestino][colunaDestino] = jogadorAtual;
@@ -192,13 +344,14 @@ void movimentar()
     }
     else
     {
-        cout << "Movimento inválido. Tente novamente." << endl;
+        cout << "Movimento invalido. Tente novamente." << endl;
         movimentar();
     }
 }
 
 void printaTabuleiro()
 {
+    textcolor(WHITE);
     system("CLS"); // Limpa tela
     // Printa primeira Linha e asciis
     cout << "   0  1  2  3  4  5  6  7" << endl;
@@ -215,7 +368,6 @@ void printaTabuleiro()
             cout << (char)(205);
         }
     }
-    int linhaIndex = 0;
     int letras = 65;
     cout << (char)(187) << endl;
 
@@ -296,8 +448,9 @@ void printaTabuleiro()
     cout << (char)(188) << endl;
 
     cout << "   0  1  2  3  4  5  6  7" << endl;
-}
+    textcolor(GREEN);
 
+}
 // o tabuleiro começa com as peças brancas em cima e as peças pretas em baixo
 void inicializarTabuleiro()
 {
@@ -323,17 +476,12 @@ void inicializarTabuleiro()
                 tabuleiro[linha][coluna] = PRETA;
 }
 
-bool comerPeca(int linhaCaptura, int colunaCaptura)
-{
-    tabuleiro[linhaCaptura][colunaCaptura] = VAZIO;
-}
-
 bool movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino, bool isMovimentando)
 {
     // Valida se as posições destino estão dentro do tabuleiro
     if (linhaDestino < 0 || linhaDestino >= TAM_TABULEIRO || colunaDestino < 0 || colunaDestino >= TAM_TABULEIRO)
     {
-        cout << "Posição de destino invade o tamanho do tabuleiro" << endl;
+        cout << "Posicao de destino invade o tamanho do tabuleiro" << endl;
         return false;
     }
 
@@ -343,7 +491,13 @@ bool movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int co
     // Verifica se a peça é do jogador atual
     if (tabuleiro[linhaOrigem][colunaOrigem] != jogadorAtual)
     {
-        cout << "A peça não é sua" << endl;
+        cout << "A peca nao e sua" << endl;
+        return false;
+    }
+
+    if (tabuleiro[linhaDestino][colunaDestino] != VAZIO)
+    {
+        cout << "Posicao de destino esta ocupada" << endl;
         return false;
     }
 
@@ -356,7 +510,7 @@ bool movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int co
         // Verifica se a posição de destino está vazia
         if (tabuleiro[linhaDestino][colunaDestino] != VAZIO)
         {
-            cout << "Posição de destino está ocupada" << endl;
+            cout << "Posicao de destino esta ocupada" << endl;
             return false;
         }
 
@@ -364,16 +518,15 @@ bool movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int co
         // Verifica se há uma peça do oponente entre a origem e o destino
         if (jogadorAtual == captura || captura == VAZIO)
         {
-            cout << "Não há peça do oponente entre a origem e o destino" << endl;
+            cout << "Nao ha peca do oponente entre a origem e o destino" << endl;
             return false;
         }
 
         // Realiza a captura (comer peça)
         if (isMovimentando == true)
         {
-            comerPeca(linhaCaptura, colunaCaptura);
+            tabuleiro[linhaCaptura][colunaCaptura] = VAZIO;
         }
-        // to do: atualizar o placar
         return true;
     }
 
@@ -387,14 +540,14 @@ bool movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int co
     // Não permite que as peças pretas desçam
     if (diferencaLinha == 1 && tabuleiro[linhaOrigem][colunaOrigem] == PRETA)
     {
-        cout << "Peça preta não pode descer" << endl;
+        cout << "Peca preta nao pode descer" << endl;
         return false;
     }
 
     // Não permite que as peças brancas subam
     if (diferencaLinha == -1 && tabuleiro[linhaOrigem][colunaOrigem] == BRANCA)
     {
-        cout << "Peça branca não pode subir" << endl;
+        cout << "Peca branca nao pode subir" << endl;
         return false;
     }
 
@@ -412,6 +565,10 @@ bool movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int co
 
     // Verifica se a peça é do jogador atual
     if (tabuleiro[linhaOrigem][colunaOrigem] != jogadorAtual)
+        return false;
+
+    // Verifica se a posição de destino está vazia
+    if (tabuleiro[linhaDestino][colunaDestino] != VAZIO)
         return false;
 
     // Verifica se é um movimento de captura (comer peça)
@@ -457,7 +614,12 @@ void escreverArquivo(string diretorio, string conteudo)
 
 string lerArquivo(string diretorio)
 {
-    fstream arquivo(diretorio, ios::in);
+    ifstream arquivo(diretorio);
+    if (!arquivo.is_open())
+    {
+        cout << "Arquivo nao encontrado" << endl;
+        return "";
+    }
     string linha;
     string conteudo;
 
@@ -468,6 +630,3 @@ string lerArquivo(string diretorio)
     arquivo.close();
     return conteudo;
 }
-
-// TODO: implementar a verificação de comer peça obrigatoriamente?????
-// TODO: verificar condição de comer mais de uma peça na mesma rodada
